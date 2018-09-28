c48e9e85b9eec29f2a033b82058ffa97028ef14c
riquejanuario <riquejanuario@jera.com.br>
2018-09-28 15:58:46 -0400

    Change generators

---
+++ lib/generators/jera_payment/install_generator.rb
@@ -0,0 +1,50 @@
+require 'rails/generators/base'
+
+module JeraPayment
+  module Generators
+
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+
+      def initializer_file
+        template 'jera_payment.rb', 'config/initializers/jera_payment.rb'
+      end
+
+      def locale_file
+        copy_file '../../../../config/locale/jera_payment.pt-BR.yml', 'config/locales/jera_payment.pt-BR.yml'
+      end
+
+      def generate_migrations
+        case self.behavior
+        when :invoke
+          generate "active_record:jera_payment", "migration"
+        when :revoke
+          Rails::Generators.invoke "active_record:jera_payment", ["migration"], behavior: :revoke
+        end
+      end
+
+      def callback_services
+        copy_subscription_files
+        copy_invoice_files
+      end
+
+      private
+      def copy_subscription_files
+        files = ['base', 'activated', 'changed', 'created', 'expired', 'renewed', 'suspended']
+
+        files.each do |file|
+          copy_file "../../../jera_payment/services/iugu/handle_callbacks/subscription/#{file}.rb", "app/services/iugu/handle_callbacks/subscription/#{file}.rb"
+        end
+      end
+
+      def copy_invoice_files
+        files = ['base', 'created', 'due', 'dunning_action', 'installment_released', 'payment_failed', 'refund', 'released', 'status_changed']
+
+        files.each do |file|
+          copy_file "../../../jera_payment/services/iugu/handle_callbacks/invoice/#{file}.rb", "app/services/iugu/handle_callbacks/invoice/#{file}.rb"
+        end
+      end
+
+    end
+  end
+end
--- lib/generators/jera_payment/jera_payment_generator.rb
+++ lib/generators/jera_payment/jera_payment_generator.rb
@@ -4,60 +4,62 @@ class JeraPaymentGenerator < Rails::Generators::NamedBase
 
   MissingModel = Class.new(Thor::Error)
 
-  def initializer_file
-    template 'jera_payment.rb', 'config/initializers/jera_payment.rb'
-  end
-
-  def locale_file
-    copy_file '../../../../config/locale/jera_payment.pt-BR.yml', 'config/locales/jera_payment.pt-BR.yml'
-  end
-
-  def callback_services
-    copy_subscription_files
-    copy_invoice_files
-  end
-
-  def generate_migrations
-		unless model_exists?
-			raise MissingModel,
-				"\n\tModel \"#{file_name.titlecase}\" doesn't exists. Please, create your Model and try again."
-		end
-
-    inject_into_file model_path, "\n\thas_one :customer, as: :customerable, class_name: 'JeraPayment::Customer'", after: '< ActiveRecord::Base'
-
-    inject_into_file model_path, "\n\thas_one :customer, as: :customerable, class_name: 'JeraPayment::Customer'", after: '< ApplicationRecord'
-
-    case self.behavior
-    when :invoke
-      generate "active_record:jera_payment", file_name
-    when :revoke
-      Rails::Generators.invoke "active_record:jera_payment", [file_name], behavior: :revoke
+  def generate_relation
+    unless model_exists?
+      raise MissingModel,
+      "\n\tModel \"#{file_name.titlecase}\" doesn't exists. Please, create your Model and try again."
     end
+
+    unless args.present?
+      raise MissingModel,
+      "\n\tYou need to pass an JeraPayment Model (Customer or/and SubAccount)"
+    end
+
+    args.each do |arg|
+      inject_into_file model_path, "\n\thas_one :#{arg.underscore}, as: :#{polymorfic_model_name(arg)}, class_name: 'JeraPayment::#{arg}'", after: '< ActiveRecord::Base'
+
+      inject_into_file model_path, "\n\thas_one :#{arg.underscore}, as: :#{polymorfic_model_name(arg)}, class_name: 'JeraPayment::#{arg}'", after: '< ApplicationRecord'
+    end
+    
   end
+
 
   private
 
-  	def model_exists?
-  		File.exist?(File.join(destination_root, model_path))
-  	end
+	def model_exists?
+		File.exist?(File.join(destination_root, model_path))
+	end
 
-	  def model_path
-		  @model_path ||= File.join("app", "models", "#{file_path}.rb")
-		end
+  def model_path
+	  @model_path ||= File.join("app", "models", "#{file_path}.rb")
+	end
 
-    def copy_subscription_files
-      files = ['base', 'activated', 'changed', 'created', 'expired', 'renewed', 'suspended']
-
-      files.each do |file|
-        copy_file "../../../jera_payment/services/iugu/handle_callbacks/subscription/#{file}.rb", "app/services/iugu/handle_callbacks/subscription/#{file}.rb"
-      end
+  def polymorfic_model_name(arg)
+    case arg
+    when "Customer"
+      return "customerable"
+    when "SubAccount"
+      return "sub_accountable"
     end
 
-    def copy_invoice_files
-      files = ['base', 'created', 'due', 'dunning_action', 'installment_released', 'payment_failed', 'refund', 'released', 'status_changed']
-
-      files.each do |file|
-        copy_file "../../../jera_payment/services/iugu/handle_callbacks/invoice/#{file}.rb", "app/services/iugu/handle_callbacks/invoice/#{file}.rb"
-      end
-    end
+    raise MissingModel, "\n\tJera Payment Model \"#{arg}\" doesn't exists. Available models: Customer and SubAccount"
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
