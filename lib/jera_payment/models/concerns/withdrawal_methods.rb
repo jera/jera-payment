6a51312de661e4596a344f700ec355cd507080d6
Henrique Januário <riquejanuario@gmail.com>
2020-01-25 21:36:39 -0400

    fix

--- lib/jera_payment/models/concerns/withdrawal_methods.rb
+++ lib/jera_payment/models/concerns/withdrawal_methods.rb
@@ -4,6 +4,10 @@ module JeraPayment
       extend ActiveSupport::Concern
 
       included do
+        def has_create_callback?
+          true
+        end
+
         def has_update_callback?
           false
         end
