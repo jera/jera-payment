ddf4a0faab22b55283e9f021097e740ae384299b
Henrique Januário <riquejanuario@gmail.com>
2020-01-25 21:18:16 -0400

    fix

--- lib/jera_payment/api/iugu/withdrawal.rb
+++ lib/jera_payment/api/iugu/withdrawal.rb
@@ -10,8 +10,12 @@ module JeraPayment
           response = post(url, body, access_token)
         end
 
-        def self.index(query = nil, access_token = nil)
-          response = get(BASE_ENDPOINT, query, access_token)
+        def self.index(query = nil, limit = nil, start = nil, access_token = nil)
+          response = HTTParty.get(
+            "#{BASE_ENDPOINT}?query=#{query}&limit=#{limit}&start=#{start}",
+            headers: set_headers(access_token),
+          )
+          parse_response(response)
         end
 
         def self.show(id, access_token = nil)
